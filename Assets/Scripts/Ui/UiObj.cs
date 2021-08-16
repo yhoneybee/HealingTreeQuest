5bbfefff38a9d12854a31d238a2d45f9208fa546
kkulbeol <chaeyh04@gmail.com>
2021-08-17 00:27:36 +0900

    UI / Quest

--- Assets/Scripts/Ui/UiObj.cs
+++ Assets/Scripts/Ui/UiObj.cs
@@ -4,213 +4,121 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class UiObj : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler, IPointerClickHandler
+public class UiObj : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public static UiObj Dragging = null;
+    public static RectTransform Dragging = null;
 
     public UiManager UI => UiManager.Instance;
 
     public Image Image = null;
-    public Coroutine CMove = null;
+    public Coroutine CSwitchingHide = null;
     public Coroutine CPreview = null;
+    public Coroutine CScalingForChild = null;
     public RectTransform RectTransform = null;
     public GridLayoutGroup GridLayoutGroup = null;
 
     public Vector2 MousePos => Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-    private bool preview = false;
-    public bool Preview
-    {
-        get
-        {
-            return preview;
-        }
-        set
-        {
-            preview = value;
-            if (CPreview != null) StopCoroutine(CPreview);
-            CPreview = StartCoroutine(EPreview());
-        }
-    }
+    public Vector2 BeginMousePos;
+    public Vector2 UiPos;
 
-    private bool hide = false;
+    private bool hide;
     public bool Hide
     {
         get { return hide; }
         set
-        {
-            hide = value;
-            ChildPreview = value;
-        }
-    }
-
-    private bool child_preview;
-
-    public bool ChildPreview
-    {
-        get { return child_preview; }
-        set 
         { 
-            child_preview = value;
-            for (int i = 0; i < transform.childCount; i++)
-                transform.GetChild(i).GetComponent<UiObj>().Preview = value;
+            hide = value;
+            if (CSwitchingHide != null) StopCoroutine(CSwitchingHide);
+            CSwitchingHide = StartCoroutine(ESwitchingHide());
         }
     }
 
+    float cell_size = 0, spacing = 0, padding = 0;
 
-    public bool Compositable = false;
-    public bool Moveable = true;
-
-
-    private void Start()
+    void Start()
     {
-    }
-
-    private void Update()
-    {
-    }
-
-    public Vector2 PivotLerpChange(Vector2 newPivot) => Vector2.Lerp(RectTransform.pivot, newPivot, Time.deltaTime * 10);
-
-    IEnumerator EMove()
-    {
-        if (Hide)
+        if (GridLayoutGroup)
         {
-            // 0.5가 가운데 기준임 그래서 0을 기준으로 함 Mathf.Abs(UI.MiddlePivot - 0.5f) + 0.5f
-            while (RectTransform.pivot.x < UI.MiddlePivot - 0.01f || RectTransform.pivot.x > UI.MiddlePivot + 0.01f)
-            {
-                RectTransform.pivot = PivotLerpChange(new Vector2(UI.MiddlePivot, RectTransform.pivot.y));
-                yield return new WaitForSeconds(0.005f);
-            }
-            RectTransform.pivot = new Vector2(UI.MiddlePivot, RectTransform.pivot.y);
+            cell_size = GridLayoutGroup.cellSize.x;
+            spacing = GridLayoutGroup.spacing.y;
+            padding = 150;
+            RectTransform.sizeDelta = new Vector2(RectTransform.sizeDelta.x, (cell_size + spacing) + padding);
         }
-        else
+    }
+
+    IEnumerator ESwitchingHide()
+    {
+        int to = Hide ? (RectTransform.anchorMin.x == 1 ? 0 : 1) : (RectTransform.anchorMin.x == 1 ? 1 : 0);
+
+        while (true)
         {
-            while (RectTransform.pivot.x > 0.1f || RectTransform.pivot.x < 0.9f)
-            {
-                if (1 - RectTransform.pivot.x <= 0.01f || RectTransform.pivot.x <= 0.01f)
-                    break;
-                RectTransform.pivot = PivotLerpChange(new Vector2(RectTransform.anchorMin.x, RectTransform.pivot.y));
-                yield return new WaitForSeconds(0.005f);
-            }
-            RectTransform.pivot = new Vector2(RectTransform.anchorMin.x, RectTransform.pivot.y);
+            if (to == 0 && RectTransform.pivot.x < 0.05f) break;
+            else if (to == 1 && RectTransform.pivot.x > 0.95f) break;
+
+            RectTransform.pivot = Vector2.Lerp(RectTransform.pivot, new Vector2(to, RectTransform.pivot.y), Time.deltaTime * 10);
+            yield return new WaitForSeconds(0.001f);
         }
 
+        RectTransform.pivot = new Vector2(to, RectTransform.pivot.y);
 
         yield return null;
     }
-
-    IEnumerator EPreview()
+    IEnumerator EScalingForChild(int child)
     {
-        if (preview)
+        while (true)
         {
-            while (Image.color.a > 0)
-            {
-                Image.color = new Color(Image.color.r, Image.color.g, Image.color.b, Image.color.a - 0.05f);
-                yield return new WaitForSeconds(0.05f);
-            }
-            Image.enabled = !preview;
-        }
-        else
-        {
-            Image.enabled = !preview;
-            while (1 > Image.color.a)
-            {
-                Image.color = new Color(Image.color.r, Image.color.g, Image.color.b, Image.color.a + 0.05f);
-                yield return new WaitForSeconds(0.05f);
-            }
-        }
+            if (RectTransform.sizeDelta.y > (cell_size + spacing) * child + padding &&
+                RectTransform.sizeDelta.y < (cell_size + spacing) * child + padding + 1)
+                break;
+            else if (RectTransform.sizeDelta.y < (cell_size + spacing) * child + padding &&
+                     RectTransform.sizeDelta.y > (cell_size + spacing) * child + padding - 1)
+                break;
 
+            RectTransform.sizeDelta = Vector2.Lerp(RectTransform.sizeDelta, new Vector2(RectTransform.sizeDelta.x, (cell_size + spacing) * child + padding), Time.deltaTime * 10);
+            yield return new WaitForSeconds(0.001f);
+        }
         yield return null;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (UI.CustomMode && Moveable)
-        {
-            Dragging = this;
-            Image.raycastTarget = false;
-            Hide = false;
-            ChildPreview = true;
-        }
+        Dragging = RectTransform;
+        BeginMousePos = MousePos;
+        UiPos = Dragging.position;
+        Image.raycastTarget = false;
     }
-
     public void OnDrag(PointerEventData eventData)
     {
-        if (Dragging && Moveable)
-        {
-            RectTransform.position = new Vector3(MousePos.x, MousePos.y, RectTransform.position.z);
+        Vector2 delta = MousePos - BeginMousePos;
 
-            if (Compositable)
-            {
-                Vector2 newAnchor = new Vector2();
+        delta += UiPos;
 
-                if (RectTransform.position.y > 0.75) // Up
-                    newAnchor = new Vector2(RectTransform.pivot.x, 1);
-                else if (RectTransform.position.y < -0.75) // Down
-                    newAnchor = new Vector2(RectTransform.pivot.x, 0);
-                else
-                    newAnchor = new Vector2(RectTransform.pivot.x, 0.5f);
-
-                if (RectTransform.position.x > 0) // Right
-                {
-                    newAnchor = new Vector2(1, newAnchor.y);
-                    GridLayoutGroup.childAlignment = TextAnchor.MiddleRight;
-                }
-                else if (RectTransform.position.x < 0) // Left
-                {
-                    newAnchor = new Vector2(0, newAnchor.y);
-                    GridLayoutGroup.childAlignment = TextAnchor.MiddleLeft;
-                }
-
-                RectTransform.anchorMin = RectTransform.anchorMax = newAnchor;
-                RectTransform.pivot = PivotLerpChange(newAnchor);
-            }
-        }
+        Dragging.position = new Vector3(delta.x, delta.y, Dragging.position.z);
     }
-
-    public void OnDrop(PointerEventData eventData)
-    {
-        if (Dragging)
-        {
-            if (Compositable)
-            {
-                Dragging.RectTransform.SetParent(transform);
-                GridLayoutGroup.enabled = false;
-                GridLayoutGroup.enabled = true;
-            }
-            else
-            {
-                Dragging.RectTransform.SetParent(UI.Canvas.transform);
-            }
-        }
-    }
-
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (Dragging)
+        Vector2 new_anchor = new Vector2(0.5f, 0.5f);
+
+        if (Dragging.position.x > 0)
         {
-            if (Compositable)
-            {
-                RectTransform.pivot = RectTransform.anchorMin;
-                RectTransform.anchoredPosition = new Vector2(0, Dragging.RectTransform.anchoredPosition.y);
-            }
-
-            ChildPreview = false;
-            Image.raycastTarget = true;
-            Dragging = null;
+            new_anchor = new Vector2(1, new_anchor.y);
+            GridLayoutGroup.childAlignment = TextAnchor.MiddleRight;
+            RectTransform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+            RectTransform.pivot = new_anchor;
         }
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (Compositable)
+        else
         {
-            Hide = !Hide;
-            if (CMove != null)
-                StopCoroutine(CMove);
-
-            CMove = StartCoroutine(EMove());
+            new_anchor = new Vector2(0, new_anchor.y);
+            GridLayoutGroup.childAlignment = TextAnchor.MiddleLeft;
+            RectTransform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            RectTransform.pivot = new Vector2(1, new_anchor.y);
         }
+
+        RectTransform.anchorMin = RectTransform.anchorMax = new_anchor;
+        RectTransform.anchoredPosition = new Vector2(0, RectTransform.anchoredPosition.y);
+
+        Image.raycastTarget = true;
+        Dragging = null;
     }
 }
