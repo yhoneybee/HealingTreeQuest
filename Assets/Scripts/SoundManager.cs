9aade310fc2724ef241586101ccb21ac8aeb9580
chae young hoon <chaeyh04@gmail.com>
2021-09-09 09:56:39 +0900

    Wa! SoundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,23 +1,109 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+public enum SoundType
+{
+    BGM,
+    EFFECT,
+    END,
+}
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource[] audioSources = new AudioSource[(int)SoundType.END];
+    Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+
     public static SoundManager Instance { get; private set; } = null;
-    public float Volume { get; set; } = 0.5f;
 
     private void Awake()
     {
-        Instance = this;
-    }
-    void Start()
-    {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+
+            GameObject sound = GameObject.Find("SoundManager");
+
+            if (sound)
+            {
+                name = "SoundManager";
+                DontDestroyOnLoad(gameObject);
+
+                string[] soundNames = Enum.GetNames(typeof(SoundType));
+
+                for (int i = 0; i < soundNames.Length - 1; i++)
+                {
+                    GameObject go = new GameObject { name = soundNames[i] };
+                    audioSources[i] = go.AddComponent<AudioSource>();
+                    go.transform.SetParent(sound.transform);
+                }
+
+                audioSources[(int)SoundType.BGM].loop = true;
+            }
+        }
     }
 
-    void Update()
+    public void StopAllSound()
     {
-        
+        foreach (var item in audioSources)
+        {
+            item.clip = null;
+            item.Stop();
+        }
+
+        audioClips.Clear();
+    }
+
+    public void Play(AudioClip audioClip, SoundType soundType = SoundType.EFFECT)
+    {
+        if (!audioClip)
+            return;
+
+        AudioSource audioSource;
+
+        if (soundType == SoundType.BGM)
+        {
+            audioSource = audioSources[(int)SoundType.BGM];
+            if (audioSource.isPlaying)
+                audioSource.Stop();
+
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource = audioSources[(int)SoundType.EFFECT];
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
+    public void Play(string path, SoundType soundType = SoundType.EFFECT) => Play(GetOrAddAudioClip(path, soundType), soundType);
+
+    AudioClip GetOrAddAudioClip(string path, SoundType soundType)
+    {
+        if (path.Contains("Sounds/") == false)
+            path = $"Sounds/{path}";
+
+        AudioClip audioClip = null;
+
+        if (soundType == SoundType.BGM)
+        {
+            audioClip = Resources.Load<AudioClip>(path);
+        }
+        else
+        {
+            if (audioClips.TryGetValue(path, out audioClip) == false)
+            {
+                audioClip = Resources.Load<AudioClip>(path);
+                audioClips.Add(path, audioClip);
+            }
+        }
+
+        if (!audioClip)
+            Debug.LogWarning($"AudioClip Missing!, path info : {path}");
+
+        return audioClip;
     }
 }
